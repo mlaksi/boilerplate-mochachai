651e3bd4cdbfc8fe7d4162bb9e50ae90d622969b
Mladenka Orlić <63567763+mlaksi@users.noreply.github.com>
2024-12-09 00:49:43 +0100

    unit tests, functional tests, and now HEADLESS BROWSER done, chai.js,mocha.js (asyncs) and zombie.js for headless browser

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -68,16 +68,21 @@ suite("Functional Tests", function () {
 
           done();
         });
-
-      done();
     });
   });
 });
 
 const Browser = require("zombie");
+Browser.site = "http://localhost:3000";
 
 suite("Functional Tests with Zombie.js", function () {
   this.timeout(5000);
+
+  const browser = new Browser();
+
+  suiteSetup((done) => {
+    return browser.visit("/", done);
+  });
 
   suite("Headless browser", function () {
     test('should have a working "site" property', function () {
@@ -88,15 +93,33 @@ suite("Functional Tests with Zombie.js", function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
+      browser.fill("surname", "Colombo").then(() => {
+        browser.pressButton("submit", () => {
+          browser.assert.success();
+          browser.assert.text("span#name", "Cristoforo");
+          browser.assert.text("span#surname", "Colombo");
+          browser.assert.elements("span#dates", 1);
+          done();
+        });
+      });
+      //assert.fail();
 
-      done();
+      //done();
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
+      browser.fill("surname", "Vespucci").then(() => {
+        browser.pressButton("submit", () => {
+          browser.assert.success();
+          browser.assert.text("span#name", "Amerigo");
+          browser.assert.text("span#surname", "Vespucci");
+          browser.assert.elements("span#dates", 1);
+          done();
+        });
+      });
+      //assert.fail();
 
-      done();
+      //done();
     });
   });
 });
